cc76c521dab4ac9c3af00526b4c4688df0472998
lexunok <lex48949@gmail.com>
2024-01-30 12:44:13 +0500

    init backend

---
+++ src/main/java/com/coursework/readersticket/ReadersTicketApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.readersticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadersTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadersTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coursework/readersticket/ReadersTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.readersticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadersTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
